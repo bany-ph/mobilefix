31737fa186fc382d5e27e4ea7085a683dfaac9c1
bany-ph <bpuertaherrera@gmail.com>
2025-10-28 23:34:21 -0500

    Added default constructor to `ResourceNotFoundException` with a predefined message, and removed unused `gitkeep` file from `dtos`.

--- src/main/java/crudactivity/mobilefix/exceptions/custom/ResourceNotFoundException.java
+++ src/main/java/crudactivity/mobilefix/exceptions/custom/ResourceNotFoundException.java
@@ -4,4 +4,9 @@ public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message) {
         super(message);
     }
+
+    public ResourceNotFoundException() {
+        super("Resource not found");
+    }
+
 }
