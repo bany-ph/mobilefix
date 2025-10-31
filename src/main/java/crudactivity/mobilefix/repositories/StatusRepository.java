d4a1b92ae2bd1243639c88d8d70272e7654ae8a8
bany-ph <bpuertaherrera@gmai.com>
2025-10-31 13:48:39 -0500

    feat: add new status repository

---
+++ src/main/java/crudactivity/mobilefix/repositories/StatusRepository.java
@@ -0,0 +1,9 @@
+package crudactivity.mobilefix.repositories;
+
+import crudactivity.mobilefix.entities.Status;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StatusRepository extends JpaRepository<Status,Long> {
+}
